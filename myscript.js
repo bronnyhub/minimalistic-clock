e3ec7eb98e665cecebcd76bb6d745a82a3dfd63a
bronnyhub <julka.bronny@gmail.com>
2023-01-25 14:13:16 +0100

    1.2
    added: center hour, full screen option, bigger font
    
    next: add
    - expand button dissapear when clicked and coming back when esc from full screen mode
    - menu button

---
+++ myscript.js
@@ -0,0 +1,37 @@
+var elem = document.documentElement;
+function openFullscreen() {
+  if(elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen(); 
+  }
+}
+
+function closeFullscreen() {
+  if(document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+} 
+
+function checkTime(i) {	
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
